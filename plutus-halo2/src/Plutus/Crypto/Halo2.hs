0db29c266c9487c152a9db145318035c68cd92bf
perturbing <thomas.vellekoop@iohk.io>
2023-11-02 14:20:03 +0100

    move transcript interface to lib

--- plutus-halo2/src/Plutus/Crypto/Halo2.hs
+++ plutus-halo2/src/Plutus/Crypto/Halo2.hs
@@ -1,3 +1,10 @@
 module Plutus.Crypto.Halo2
-( 
+( module X
 ) where
+
+import Plutus.Crypto.Halo2.Transcript as X
+    ( Transcript
+    , squeezeChallange
+    , addScalarToTranscript
+    , addPointToTranscript
+    )
---
+++ plutus-halo2/src/Plutus/Crypto/Halo2/Transcript.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE NoImplicitPrelude  #-}
+{-# LANGUAGE OverloadedStrings  #-}
+
+module Plutus.Crypto.Halo2.Transcript 
+( Transcript
+, squeezeChallange
+, addScalarToTranscript
+, addPointToTranscript
+) where
+
+import PlutusTx.Prelude
+    ( BuiltinBLS12_381_G1_Element,
+      BuiltinByteString,
+      ($),
+      (.),
+      fst,
+      snd,
+      blake2b_256,
+      byteStringToInteger,
+      integerToByteString,
+      modulo,
+      Semigroup((<>)) ) 
+import PlutusTx.Builtins
+    ( BuiltinBLS12_381_G1_Element,
+      BuiltinByteString,
+      blake2b_256,
+      byteStringToInteger,
+      integerToByteString ) 
+import Plutus.Crypto.BlsUtils
+    ( Scalar(..),
+      bls12_381_field_prime,
+      Fp(unFp),
+      mkScalar,
+      unCompressG1Point )
+
+type Transcript = BuiltinByteString
+
+{-# INLINEABLE squeezeChallange #-}
+squeezeChallange :: Transcript -> (Scalar, Transcript)
+squeezeChallange bs = (mkScalar (byteStringToInteger (blake2b_256 (bs <> "\x00")) `modulo` bls12_381_field_prime), bs <> "\x00")
+
+{-# INLINEABLE addScalarToTranscript #-}
+addScalarToTranscript :: Transcript -> Scalar -> Transcript
+addScalarToTranscript bs s = bs <> "\x02" <> integerToByteString (unScalar s)
+
+{-# INLINEABLE addPointToTranscript #-}
+addPointToTranscript :: Transcript -> BuiltinBLS12_381_G1_Element -> Transcript
+addPointToTranscript bs p = bs <> "\x01" <> integerToByteString x 
+                             <> integerToByteString y
+                where x = unFp . fst . unCompressG1Point $ p
+                      y = unFp . snd . unCompressG1Point $ p
